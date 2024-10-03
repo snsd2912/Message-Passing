fb17ab06bdbe8b2730ab2dca22d9c57cba13744c
snsd2912 <lesang407407@gmail.com>
2024-10-03 18:32:25 +0700

    refactor

---
+++ modules/location-consumer/location-consumer.py
@@ -0,0 +1,45 @@
+import json
+import os
+
+from kafka import KafkaConsumer
+from sqlalchemy import create_engine
+
+KAFKA_SERVER = os.environ["KAFKA_SERVER"]
+TOPIC_NAME = os.environ["TOPIC_NAME"]
+
+DB_USERNAME = os.environ["DB_USERNAME"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_NAME = os.environ["DB_NAME"]
+
+
+consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=[KAFKA_SERVER])
+print('Start listening topic: ' + TOPIC_NAME)
+
+def connect_db():
+    engine = create_engine(f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=True)
+    return engine.connect()
+
+def consume_and_save():
+    connection = connect_db()
+    try:
+        print("Kafka consumer started with persistent DB connection...")
+
+        for message in consumer:
+            data = message.value.decode('utf-8')
+            location = json.loads(data)
+            person_id = int(location["person_id"])
+            longitude, latitude = int(location["longitude"]), int(location["latitude"])
+
+            insert = "INSERT INTO location (person_id, coordinate) VALUES ({}, ST_Point({}, {}))".format(person_id, latitude, longitude)
+
+            print(insert)
+            connection.execute(insert)
+
+    except KeyboardInterrupt:
+        print("Consumer interrupted, shutting down...")
+    finally:
+        connection.close()
+
+consume_and_save()
